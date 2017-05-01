bc7db703f03ca5c1667a110840c9209a3ad67a9c
Aldo Rey Balagulan <mrawesomer34@gmail.com>
2017-05-01 21:18:56 +0800

    Initial work on getting ReactJS to work
    - React should technically be working now
    - Webpack has been installed so that we can easily manage our output JS
    files and maybe our stylesheets later

--- microsite/views.py
+++ microsite/views.py
@@ -3,8 +3,10 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("This is the home page")
+    template = loader.get_template('microsite/index.html')
+    return HttpResponse(template.render())
