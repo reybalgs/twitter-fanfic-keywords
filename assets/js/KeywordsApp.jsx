7a436c58e4683b2c3b503aaf9ae6ea4fdb9b81c2
Aldo Rey Balagulan <mrawesomer34@gmail.com>
2017-05-05 23:57:58 +0800

    Some small work on the layout

--- assets/js/KeywordsApp.jsx
+++ assets/js/KeywordsApp.jsx
@@ -6,17 +6,24 @@ import KeywordsAppBar from './KeywordsAppBar';
 export default class KeywordsApp extends Component {
   constructor(props) {
     super(props);
-    this.person = { name: 'Azu-nyan' };
   }
 
   render() {
     return (
       <div>
         <KeywordsAppBar></KeywordsAppBar>
+        <div className="hero-area">
+          <div className="container">
+            <h2 className="hero-title">Start Generating!</h2>
+          </div>
+        </div>
         <div className="container">
-          <h1>
-          Goodbye, {this.person.name}!
-          </h1>
+          <h2>What is this?</h2>
+          <p>
+            This app is a small demonstration of a Twitter bot that generates
+            keywords (from multiple sources) that people can use for fan
+            fiction (or generally just any kind of prose really) material.
+          </p>
         </div>
       </div>
     )
